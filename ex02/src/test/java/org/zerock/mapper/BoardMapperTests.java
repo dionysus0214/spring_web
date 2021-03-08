48877608f18bfecd47175247ff868a20f277714d
dionysus0214 <hiba0214@naver.com>
2021-03-08 12:03:41 +0900

    ch15 검색 처리

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -22,98 +22,96 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class BoardMapperTests {
 
-	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper;
+  @Setter(onMethod_ = @Autowired)
+  private BoardMapper mapper;
 
-	@Test
-	public void testGetList() {
+  @Test
+  public void testGetList() {
 
-		mapper.getList().forEach(board -> log.info(board));
+    mapper.getList().forEach(board -> log.info(board));
 
-	}
+  }
 
-	@Test
-	public void testInsert() {
+  @Test
+  public void testInsert() {
 
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글");
-		board.setContent("새로 작성하는 내용");
-		board.setWriter("newbie");
+    BoardVO board = new BoardVO();
+    board.setTitle("새로 작성하는 글");
+    board.setContent("새로 작성하는 내용");
+    board.setWriter("newbie");
 
-		mapper.insert(board);
+    mapper.insert(board);
 
-		log.info(board);
-	}
+    log.info(board);
+  }
 
-	@Test
-	public void testInsertSelectKey() {
+  @Test
+  public void testInsertSelectKey() {
 
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글 select key");
-		board.setContent("새로 작성하는 내용 select key");
-		board.setWriter("newbie");
+    BoardVO board = new BoardVO();
+    board.setTitle("새로 작성하는 글 select key");
+    board.setContent("새로 작성하는 내용 select key");
+    board.setWriter("newbie");
 
-		mapper.insertSelectKey(board);
+    mapper.insertSelectKey(board);
 
-		log.info(board);
-	}
+    log.info(board);
+  }
 
-	@Test
-	public void testRead() {
+  @Test
+  public void testRead() {
 
-		BoardVO board = mapper.read(4L);
+    BoardVO board = mapper.read(4L);
 
-		log.info(board);
+    log.info(board);
 
-	}
+  }
 
-	@Test
-	public void testDelete() {
+  @Test
+  public void testDelete() {
 
-		log.info("DELETE COUNT: " + mapper.delete(5L));
-	}
+    log.info("DELETE COUNT: " + mapper.delete(5L));
+  }
 
-	@Test
-	public void testUpdate() {
+  @Test
+  public void testUpdate() {
 
-		BoardVO board = new BoardVO();
-		board.setBno(5L);
-		board.setTitle("수정된 제목");
-		board.setContent("수정된 내용");
-		board.setWriter("user00");
+    BoardVO board = new BoardVO();
+    board.setBno(5L);
+    board.setTitle("수정된 제목");
+    board.setContent("수정된 내용");
+    board.setWriter("user00");
 
-		int count = mapper.update(board);
-		log.info("UPDATE COUNT: " + count);
+    int count = mapper.update(board);
+    log.info("UPDATE COUNT: " + count);
 
-	}
+  }
 
-	@Test
-	public void testPaging() {
+  @Test
+  public void testPaging() {
 
-		Criteria cri = new Criteria();
-		
-	    //10개씩 3페이지 
-	    cri.setPageNum(3);
-	    cri.setAmount(10);
+    Criteria cri = new Criteria();
 
+    // 10개씩 3페이지
+    cri.setPageNum(3);
+    cri.setAmount(10);
 
-		List<BoardVO> list = mapper.getListWithPaging(cri);
+    List<BoardVO> list = mapper.getListWithPaging(cri);
 
-		list.forEach(board -> log.info(board));
+    list.forEach(board -> log.info(board));
 
-	}
-//	
-//	  @Test
-//	  public void testSearch() {
-//
-//	    Criteria cri = new Criteria();
-//	    cri.setKeyword("키워드");
-//	    cri.setType("TCW");
-//
-//	    List<BoardVO> list = mapper.getListWithPaging(cri);
-//
-//	    list.forEach(board -> log.info(board));
-//	  }
-//
+  }
+
+  @Test
+  public void testSearch() {
+
+    Criteria cri = new Criteria();
+    cri.setKeyword("키워드");
+    cri.setType("TCW");
+
+    List<BoardVO> list = mapper.getListWithPaging(cri);
+
+    list.forEach(board -> log.info(board));
+  }
 
 }
